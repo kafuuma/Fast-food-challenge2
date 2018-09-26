5058f4deb12060adea6dfd80fbf1694b27a3d24f
kafuuma <arkafuuma@gmail.com>
2018-09-26 09:20:10 +0300

    (Refactoring code to store objects intsead of dictionaries) This commit delivers part of the refactored code to implement real OOP, store objects to data structure [#160760351]

--- app/datastruct.py
+++ app/datastruct.py
@@ -1,29 +1,46 @@
 
 class DataStruct:
     """
-    This class implements the data structure with 3 liststo store dictionary items 
+    This class implements the data structure with 3 lists to store dictionary items 
     """
     menu  =   []
     orders =  []
     users =   []
     
     def add_orders(self, order):
-        self.orders.append(order)
-            
+        for menu_item in self.menu:
+            if menu_item.menu_id == order.menu_id:
+                self.orders.append(order)
+                return True
+        return False
 
     def add_menu(self, menu):
         self.menu.append(menu)
 
-    def add_user(self, user_id, full_name, password, email, contact, user_role):
-        user = {
-                "user_id": user_id,
-                "name": full_name,
-                "password": password,
-                "email": email,
-                "contact": contact,
-                "user_role": user_role
-            }
+    def add_user(self, user):
         self.users.append(user)
+
+    def get_orders(self):
+        if len(self.orders) >=1:
+            return self.orders
+        return None
+
+    def get_order(self, id):
+        for order in self.orders:
+            if order.order_id == id:
+                return order
+        return None
+    
+    def modify_order_staus(self, status, id):
+        index = 0
+        for order in self.orders:
+            if order.order_id == id:
+                self.orders[index].status = status
+                return self.orders[index]
+            index = index+1
+        return None
+
+
 
     def empty_data(self):
         self.menu[:] = []
