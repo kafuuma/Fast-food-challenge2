cdd353e866213e2ceeda63887a25f2c0e130529c
kafuuma <arkafuuma@gmail.com>
2018-09-28 21:08:56 +0300

    ch(Coreect OOP): This commit delivers LFA's feedback I correct code in order and menu class [Fixes #160845954]

--- app/menu.py
+++ app/menu.py
@@ -6,14 +6,13 @@ class Menu:
     def __init__(self,menu_name="", description="", menu_id =0):
         self.menu_name = menu_name
         self.description = description
-        self.menu_id = menu_id
-
-    def save_menu_item(self):
         if len(store.menu) >=1:
             self.menu_id = store.menu[-1].menu_id+1
         else:
             self.menu_id = 1
-        store.add_menu(Menu(self.menu_name, self.description, self.menu_id))
+
+    def save_menu_item(self):
+        store.add_menu(self)
 
     def fetch_menu_items(self):
         pass
--- app/orders.py
+++ app/orders.py
@@ -9,19 +9,14 @@ class Orders:
         self.user_name= user_name
         self.status = "pending"
         self.menu_id = menu_id
-        self.order_id = order_id
-     
-
-    def save_order(self):
-        """This memthod appends orders to the Data structure"""
         if len(store.orders) >=1:
             self.order_id = store.orders[-1].order_id+1
         else:
             self.order_id = 1
-        store.add_orders(Orders(
-                  self.user_name, self.menu_id,
-                  self.status,self.order_id,
-                )
-            )
+     
+
+    def save_order(self):
+        """This memthod appends orders to the Data structure"""
+        store.add_orders(self)
 
 
